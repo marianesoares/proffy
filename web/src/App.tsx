f09b3dcbd4199d5a0e66ddb06b60449a5fea67cc
Mariane Soares <marianesoaress1998@gmail.com>
2020-08-10 00:46:19 -0300

    web app

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import './assets/styles/global.css';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+       <Routes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
